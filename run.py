8da82ca4d0fd64389396b13fef01b4654419491d
Sizhon <Simon.Zhu001@umb.edu>
2022-12-21 17:15:07 -0500

    a

--- run.py
+++ run.py
@@ -3,6 +3,8 @@ import time
 
 
 def main():
+    led = machine.Pin("LED", machine.Pin.OUT)
+    led.off()
     # read settings.json
     from lib.at_client import io_util
     ssid, password, atSign = io_util.read_settings()
@@ -19,11 +21,9 @@ def main():
     atClient = at_client.AtClient(atSign, writeKeys=False) # set writeKeys=False once you've wrote your keys at least once.
     atClient.pkam_authenticate(verbose=True)
     del at_client
-
     
-
     # Start of sending data
-    led = machine.Pin("LED", machine.Pin.OUT)
+    
     led.on()
     ProxSensor = machine.ADC(26)
     MotionSensor = machine.ADC(27)
@@ -31,9 +31,9 @@ def main():
 
     while True:
         readprox = str(not(round(ProxSensor.read_u16()*convert)))
-        print(f"Proximity: " , readprox)
-        readmotion = str(not(not(round(MotionSensor.read_u16()*convert))))
-        print(f"Motion: " , readmotion)
+        #print(f"Proximity: " , readprox)
+        readmotion = str(bool(round(MotionSensor.read_u16()*convert)))
+        #print(f"Motion: " , readmotion)
         atClient.put_public("inProximity" , readprox)
         atClient.put_public("inMotion" , readmotion)
 
